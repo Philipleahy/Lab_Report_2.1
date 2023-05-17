f1cf8047a2637fd09b043103685fb9aa15c150c5
Philipleahy <philip.leahy@HOTMAIL.COM>
2023-05-17 17:46:47 +0000

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,12 +1,22 @@
+//Lab Report Serial Communications Experiment
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
+//Declarations
+Serial pc(USBTX, USBRX); // tx, rx
+PwmOut led(LED1);
+float brightness = 0.0;
 int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+ pc.printf("Press 'u' to turn LED1 brightness up, 'd' to turn it down\n");
+ while(1) {
+ char c = pc.getc();
+ if((c == 'u') && (brightness < 0.5)) {
+ brightness += 0.01;
+ led = brightness;
+ pc.putc('^');
+ }
+ if((c == 'd') && (brightness > 0.0)) {
+ brightness -= 0.01;
+ led = brightness;
+ pc.putc('v');
+ }
+ }
 }
